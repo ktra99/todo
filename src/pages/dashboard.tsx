84228a4b0012079dda6af76a5f16e5a540a66836
Kenny Tran <kenny@telnect.com>
2023-02-11 13:06:05 +0100

    add navbar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -31,6 +31,42 @@ import { useRouter } from "next/router";
 import { FormEvent, Fragment, useEffect, useMemo, useState } from "react";
 import { format, formatDistance } from "date-fns";
 
+function Navbar() {
+  return (
+    <div className="p-4">
+      <nav className="flex items-center justify-between" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="https://www.ktra99.dev/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Ktra99</span>
+            <img className="h-6 w-6" src="/logo.png" alt="avatar" />
+          </a>
+        </div>
+        <div className="flex flex-1 justify-end">
+          <a
+            href="https://github.com/ktra99/todo"
+            className="text-sm font-semibold leading-6 text-gray-900"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <svg
+              className="h-5 w-5 text-gray-400 hover:text-gray-500"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
 function Account() {
   const [user] = useAtom(userAtom);
   const [tasks] = useAtom(taskAtom);
@@ -667,7 +703,8 @@ export default function Dashboard() {
         <title>To-do - Dashboard</title>
       </Head>
       {loading && <Loading />}
-      <div className="relative my-10 flex min-h-full flex-col">
+      <Navbar />
+      <div className="relative mb-10 flex min-h-full flex-col">
         <div className="mx-auto w-full max-w-7xl flex-grow lg:flex xl:px-8">
           <div className="min-w-0 flex-1 bg-white xl:flex">
             <Account />
